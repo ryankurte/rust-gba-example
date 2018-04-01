f5a5a4a34b5514922b9fa130cac7de04dab83640
Ryan Kurte <ryankurte@gmail.com>
2018-04-02 00:40:53 +1200

    100% more rainbow

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,32 @@
 extern crate gba as gba;
 use gba::gfx::{Mode3, Colour};
 
+const max: u8 = 0x1F;
+
 fn main() {
     let g = Mode3::new();
 
     g.enable();
 
+    let rainbow: [Colour; 6] = [
+        Colour::rgb(max, 0, 0),
+        Colour::rgb(max, max/2, 0),
+        Colour::rgb(max, max, 0),
+        Colour::rgb(0, max, 0),
+        Colour::rgb(0, 0, max),
+        Colour::rgb(max/2, 0, max),
+    ];
+
+
     for x in 0..240 {
-        for y in 45..55 {
-            g.set(x, y, Colour::rgb(0xFF, 0x00, 0x00));
-        }
 
-        for y in 65..75 {
-            g.set(x, y, Colour::rgb(0x00, 0xFF, 0x00));
-        }
+        let start = 160 / 2 - rainbow.len() * 10 / 2;
 
-        for y in 85..95 {
-            g.set(x, y, Colour::rgb(0x00, 0x00, 0xFF));
+        for c in 0..rainbow.len() {
+            let offset = start + 10 * c;
+            for y in offset..offset+10 {
+                g.set(x, y, rainbow[c].clone());
+            }
         }
     }
 
