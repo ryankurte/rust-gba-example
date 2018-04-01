6aec1248fbc92144567ebd4d00cdf4a44d66b108
Ryan Kurte <ryankurte@gmail.com>
2018-04-02 00:19:34 +1200

    Updated deps

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,8 @@
 extern crate gba as gba;
 use gba::gfx::{Mode3, Colour};
 
-use core::ptr;
-use core::slice;
-
-// [0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]
-static RODATA: &str = "Hello, world!";
-
-static mut BSS: u32 = 0;
-static mut DATA: u32 = 1;
-
 fn main() {
-    let mut g = Mode3::new();
+    let g = Mode3::new();
 
     g.enable();
 
