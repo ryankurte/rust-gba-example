ed3e8c39c412b56638f03df837647b54ecbcf59b
Ryan Kurte <ryankurte@gmail.com>
2018-05-30 11:46:54 +1200

    Up and down now scale rainbow with vsync and buffer swap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(lang_items)]
+#![feature(asm)]
 #![no_std]
 
 extern crate gba as gba;
@@ -10,26 +11,34 @@ use gba::graphics::helpers::{rainbow, rainbow2};
 
 const max: u8 = 0x1F;
 
+fn wait(n: usize) {
+    for _i in 0 .. n {
+        unsafe {
+            asm!("nop");
+        }
+    }
+}
+
 fn main() {
     let mut io = IO::new();
     let mut m = Graphics::<Mode4>::new();
     let mut g = m.active();
 
+    let mut width = 10;
+
     loop {
         io.update();
-
-        let mut width = 10;
-        if io.is_pressed(Keys::Up) && width < 40 {
+        if io.is_pressed(Keys::Up) && width < 20 {
             width += 1;
         }
         if io.is_pressed(Keys::Down) && width > 2 {
             width -= 1;
         }
-
+    
         g.clear();
         rainbow2(g, width);
 
-        while !g.vsync() {};
+        while g.vblank() {};
         g.swap();
     }
 
